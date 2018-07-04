b6553b123952ddfbeaf25dbd7ce8643f1ce3c014
punoqun <bahattinmaral@gmail.com>
2018-07-04 15:38:52 +0300

    Added Search to the List activity page.

--- app/src/main/java/com/calendar/gitbusters/calendarapp/ListAppointmentsActivity.java
+++ app/src/main/java/com/calendar/gitbusters/calendarapp/ListAppointmentsActivity.java
@@ -1,13 +1,12 @@
 package com.calendar.gitbusters.calendarapp;
-
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -15,6 +14,8 @@ public class ListAppointmentsActivity extends AppCompatActivity {
 
     private ArrayList<Appointment> aptList = null;
     private LinearLayout ll = null;
+    private TextInputEditText searchbar;
+    private String search="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,9 @@ public class ListAppointmentsActivity extends AppCompatActivity {
         aptList = (ArrayList<Appointment>) getIntent().getSerializableExtra("apt-list");
         ll = (LinearLayout) findViewById(R.id.listAppointmentsLL);
 
+
+        searchbar = (TextInputEditText)findViewById(R.id.SearchBar);
+        searchbar.addTextChangedListener(watchSearch);
         // Fill the actual list (LinearLayout) using event info
         if(aptList.size() > 0)
             setListElements();
@@ -37,14 +41,18 @@ public class ListAppointmentsActivity extends AppCompatActivity {
 
     private void setListElements()
     {
+        ll.removeAllViewsInLayout();
         for(Appointment i : aptList)
         {
+
+            String txt = i.getTitle() + "\n" + i.getNotes() + "\n" + i.getDate()+ ": " + i.getTime();
+            if(!txt.contains(search)) continue;
             // Create a textview that displays event info and a button
             // that TODO will let user edit event info.
             TextView tv = new TextView(this);
             Button b = new Button(this);
-            tv.setText(i.getTitle() + "\n" + i.getNotes() + "\n" + i.getDate()
-                    + ": " + i.getTime());
+
+            tv.setText(txt);
             b.setText("EDIT");
 
             // Do the below for a relatively more beautiful list layout.
@@ -63,5 +71,28 @@ public class ListAppointmentsActivity extends AppCompatActivity {
 
             ll.addView(horizontalLL);
         }
+        if(aptList.size() == 0)
+        {
+            TextView tv = new TextView(this);
+            tv.setText("There seems to be nothing here...");
+            ll.addView(tv);
+        }
     }
+    private TextWatcher watchSearch = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            search = searchbar.getText().toString();
+            setListElements();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            search = searchbar.getText().toString();
+            setListElements();
+        }
+    };
 }
